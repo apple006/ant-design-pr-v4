a9eefe50188e68ad29ff2e3f7749d90e9987cc9f
1169794338@qq.com <1169794338@qq.com>
2019-10-29 21:02:35 +0800

    同步官方最新代码

--- src/pages/list/search/articles/index.tsx
+++ src/pages/list/search/articles/index.tsx
@@ -99,19 +99,20 @@ class Articles extends Component<ArticlesProps> {
       },
     };
 
-    const loadMore = list.length > 0 && (
-      <div style={{ textAlign: 'center', marginTop: 16 }}>
-        <Button onClick={this.fetchMore} style={{ paddingLeft: 48, paddingRight: 48 }}>
-          {loading ? (
-            <span>
-              <Icon type="loading" /> 加载中...
-            </span>
-          ) : (
-            '加载更多'
-          )}
-        </Button>
-      </div>
-    );
+    const loadMore =
+      list.length > 0 ? (
+        <div style={{ textAlign: 'center', marginTop: 16 }}>
+          <Button onClick={this.fetchMore} style={{ paddingLeft: 48, paddingRight: 48 }}>
+            {loading ? (
+              <span>
+                <Icon type="loading" /> 加载中...
+              </span>
+            ) : (
+              '加载更多'
+            )}
+          </Button>
+        </div>
+      ) : null;
 
     return (
       <>

94b5c0a2ad33ea3ea69ea32ad0a0883d790227d9
1169794338@qq.com <1169794338@qq.com>
2019-12-15 22:40:36 +0800

    Pagination组件

---
+++ src/pages/demo/Pagination.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { GridContent } from '@ant-design/pro-layout';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Index extends Component {
+  render() {
+    return <GridContent>fdsafas</GridContent>;
+  }
+}
+
+export default Index;
